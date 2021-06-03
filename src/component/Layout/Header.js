36d0253f5b759b2c7cf660c969fafc4d74a29b2a
Narayan Kumar Jha <narayank9312@gmail.com>
2021-06-03 14:20:17 +0530

    managing cart and modal state
    Signed-off-by: Narayan Kumar Jha <narayank9312@gmail.com>

--- src/component/Layout/Header.js
+++ src/component/Layout/Header.js
@@ -1,12 +1,12 @@
 import classes from './Header.module.css';
 import mealsImage from '../../utils/images/meals.jpg';
 import HeaderCartButton from './HeaderCartButton';
-const Header = () => {
+const Header = (props) => {
   return (
     <div>
       <div className={classes.header}>
         <h1>NavneetMeals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </div>
       <div className={classes['main-image']}>
         <img src={mealsImage} alt="delicious food" />
