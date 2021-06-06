712b5cbf45352c0245b14dba8b26bf71876c151e
Narayan Kumar Jha <narayank9312@gmail.com>
2021-06-07 02:19:26 +0530

    firebase setup for user
    Signed-off-by: Narayan Kumar Jha <narayank9312@gmail.com>

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -1,22 +1,52 @@
+import React, {useContext, useState} from 'react';
+
 import Modal from '../UI/Modal';
-import classes from './Cart.module.css';
-import {useContext} from 'react';
-import CartContext from '../../store/cart-context';
 import CartItem from './CartItem';
+import classes from './Cart.module.css';
+import CartContext from '../../store/cart-context';
+import Checkout from './Checkout';
 
 const Cart = (props) => {
-  const cartctx = useContext(CartContext);
-  const totalAmount = `$${cartctx.totalAmount.toFixed(2)}`;
-  const hasItem = cartctx.items.length > 0;
+  const [isCheckout, setIsCheckout] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [didSubmit, setDidSubmit] = useState(false);
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
   const cartItemRemoveHandler = (id) => {
-    cartctx.removeItem(id);
+    cartCtx.removeItem(id);
   };
+
   const cartItemAddHandler = (item) => {
-    cartctx.addItem(item);
+    cartCtx.addItem(item);
   };
+
+  const orderHandler = () => {
+    setIsCheckout(true);
+  };
+
+  const submitOrderHandler = async (userData) => {
+    setIsSubmitting(true);
+    await fetch(
+      'https://food-order-4b359-default-rtdb.firebaseio.com/order.json',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          user: userData,
+          orderedItems: cartCtx.items,
+        }),
+      }
+    );
+    setIsSubmitting(false);
+    setDidSubmit(true);
+    cartCtx.clearCart();
+  };
+
   const cartItems = (
     <ul className={classes['cart-items']}>
-      {cartctx.items.map((item) => (
+      {cartCtx.items.map((item) => (
         <CartItem
           key={item.id}
           name={item.name}
@@ -24,27 +54,58 @@ const Cart = (props) => {
           price={item.price}
           onRemove={cartItemRemoveHandler.bind(null, item.id)}
           onAdd={cartItemAddHandler.bind(null, item)}
-        >
-          {item.name}
-        </CartItem>
+        />
       ))}
     </ul>
   );
 
-  return (
-    <Modal onClose={props.onClose}>
+  const modalActions = (
+    <div className={classes.actions}>
+      <button className={classes['button--alt']} onClick={props.onClose}>
+        Close
+      </button>
+      {hasItems && (
+        <button className={classes.button} onClick={orderHandler}>
+          Order
+        </button>
+      )}
+    </div>
+  );
+
+  const cartModalContent = (
+    <React.Fragment>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
+      {isCheckout && (
+        <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose} />
+      )}
+      {!isCheckout && modalActions}
+    </React.Fragment>
+  );
+
+  const isSubmittingModalContent = <p>Sending order data...</p>;
+
+  const didSubmitModalContent = (
+    <React.Fragment>
+      <p>Successfully sent the order!</p>
       <div className={classes.actions}>
-        <button className={classes['button--alt']} onClick={props.onClose}>
+        <button className={classes.button} onClick={props.onClose}>
           Close
         </button>
-        {hasItem && <button className={classes.button}>Order</button>}
       </div>
+    </React.Fragment>
+  );
+
+  return (
+    <Modal onClose={props.onClose}>
+      {!isSubmitting && !didSubmit && cartModalContent}
+      {isSubmitting && isSubmittingModalContent}
+      {!isSubmitting && didSubmit && didSubmitModalContent}
     </Modal>
   );
 };
+
 export default Cart;
--- src/component/Meals/MealItem/MealItem.js
+++ src/component/Meals/MealItem/MealItem.js
@@ -1,13 +1,16 @@
-import classes from './MealItem.module.css';
 import {useContext} from 'react';
+
 import MealItemForm from './MealItemForm';
+import classes from './MealItem.module.css';
 import CartContext from '../../../store/cart-context';
+
 const MealItem = (props) => {
-  const cartctx = useContext(CartContext);
+  const cartCtx = useContext(CartContext);
+
   const price = `$${props.price.toFixed(2)}`;
 
-  const addItemToCartHandler = (amount) => {
-    cartctx.addItem({
+  const addToCartHandler = (amount) => {
+    cartCtx.addItem({
       id: props.id,
       name: props.name,
       amount: amount,
@@ -19,11 +22,11 @@ const MealItem = (props) => {
     <li className={classes.meal}>
       <div>
         <h3>{props.name}</h3>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>{price}</div>
       </div>
-      <div className={classes.description}>{props.description}</div>
-      <div className={classes.price}>{price}</div>
       <div>
-        <MealItemForm id={props.id} onAddToCart={addItemToCartHandler} />
+        <MealItemForm onAddToCart={addToCartHandler} />
       </div>
     </li>
   );
